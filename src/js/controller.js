801650757bc441c0873e9adc19c444bec424688e
Ursachianu Ionut-Alexandru <tzmic1@gmail.com>
2023-01-03 10:48:38 +0200

    Implementing bookmarks

--- src/js/controller.js
+++ src/js/controller.js
@@ -2,6 +2,7 @@ import * as model from './model';
 import recipeView from './views/recipeView';
 import searchView from './views/searchView';
 import resultsView from './views/resultsView';
+import bookmarksView from './views/bookmarksView';
 import paginationView from './views/paginationView';
 
 import 'core-js/stable';
@@ -20,6 +21,7 @@ const controlRecipes = async function () {
 
     // 0) Update results view to mark selected search result
     resultsView.update(model.getSearchResultsPage());
+    bookmarksView.update(model.state.bookmarks);
 
     // 1) Loading recipe
     await model.loadRecipe(id);
@@ -64,13 +66,25 @@ const controlServings = function (newServings) {
   // Update the recipe servings (in state)
   model.updateServings(newServings);
   // Update the recipe view
-  // recipeView.render(model.state.recipe);
   recipeView.update(model.state.recipe);
+};
+
+const controlAddBookmark = function () {
+  // Add/remove bookmark
+  if (!model.state.recipe.bookmarked) model.addBookmark(model.state.recipe);
+  else model.deleteBookmark(model.state.recipe.id);
+
+  // Update recipe view
+  recipeView.update(model.state.recipe);
+
+  // Render bookmarks
+  bookmarksView.render(model.state.bookmarks);
 };
 
 const init = function () {
   recipeView.addHandlerRender(controlRecipes);
   recipeView.addHandlerUpdateServings(controlServings);
+  recipeView.addHandlerAddBookmark(controlAddBookmark);
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
 };
--- src/js/model.js
+++ src/js/model.js
@@ -1,21 +1,29 @@
 import { API_URL, RES_PER_PAGE } from './config';
 import { getJSON } from './helpers';
 
+// `state` is an object that holds the current state of the application
 export const state = {
-  recipe: {},
+  recipe: {}, // current recipe being displayed
   search: {
-    query: ``,
-    page: 1,
-    results: [],
-    resultsPerPage: RES_PER_PAGE,
+    // current search results and query
+    query: ``, // search query
+    page: 1, // current page of search results
+    results: [], // array of search results
+    resultsPerPage: RES_PER_PAGE, // number of results per page
   },
+  bookmarks: [],
 };
 
+// Loads and sets the current recipe by its id
 export const loadRecipe = async function (id) {
   try {
+    // fetch recipe data from API
     const data = await getJSON(`${API_URL}${id}`);
 
+    // destructure recipe data from response
     const { recipe } = data.data;
+
+    // update state with recipe data
     state.recipe = {
       id: recipe.id,
       title: recipe.title,
@@ -26,43 +34,86 @@ export const loadRecipe = async function (id) {
       cookingTime: recipe.cooking_time,
       ingredients: recipe.ingredients,
     };
+
+    if (state.bookmarks.some(bookmark => bookmark.id === id))
+      state.recipe.bookmarked = true;
+    else state.recipe.bookmarked = false;
   } catch (error) {
     console.error(`${error} 💥`);
     throw error;
   }
 };
 
+// Loads search results for a given query and updates state
 export const loadSearchResults = async function (query) {
   try {
+    // update search query in state
     state.search.query = query;
 
+    // fetch search results from API
     const data = await getJSON(`${API_URL}?search=${query}`);
 
+    // map search results to an array of objects with relevant data
     state.search.results = data.data.recipes.map(rec => ({
       id: rec.id,
       title: rec.title,
       publisher: rec.publisher,
       image: rec.image_url,
     }));
+
+    // Reset page on a new submit search request
+    state.search.page = 1;
   } catch (error) {
     console.error(`${error} 💥`);
     throw error;
   }
 };
 
+// Returns the current page of search results
 export const getSearchResultsPage = function (page = state.search.page) {
+  // update current page in state
   state.search.page = page;
+
+  // calculate start and end indices of search results to display
   const start = (page - 1) * state.search.resultsPerPage; // 0
   const end = page * state.search.resultsPerPage; // 9
 
+  // return a slice of the search results
   return state.search.results.slice(start, end);
 };
 
+// Updates the ingredient quantities for a new serving size
 export const updateServings = function (newServings) {
+  // update ingredient quantities to reflect new serving size
   state.recipe.ingredients.forEach(ing => {
     ing.quantity = (ing.quantity * newServings) / state.recipe.servings;
-    // newQt = oldQt * newServings / oldQt
   });
-
+  // update state with new serving size
   state.recipe.servings = newServings;
 };
+
+// Add a bookmark to the state and update the bookmarked status of the current recipe, if necessary
+export function addBookmark(recipe) {
+  // Add bookmark to state
+  state.bookmarks.push(recipe);
+
+  // Update bookmarked status of current recipe
+  updateBookmarkedStatus(recipe, true);
+}
+
+// Remove a bookmark from the state and update the bookmarked status of the current recipe, if necessary
+export function deleteBookmark(id) {
+  // Find the index of the bookmark to delete
+  const index = state.bookmarks.findIndex(el => el.id === id);
+
+  // Remove the bookmark from the state
+  state.bookmarks.splice(index, 1);
+
+  // Update bookmarked status of current recipe
+  updateBookmarkedStatus(state.recipe, false);
+}
+
+// Update the bookmarked status of a recipe
+function updateBookmarkedStatus(recipe, newStatus) {
+  recipe.bookmarked = newStatus;
+}
